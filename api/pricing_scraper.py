8451dae99dc44392a08b3863807c1c27b4dc7547
helidonashabani <helidona.shabani@gmail.com>
2025-12-21 12:39:38 -0500

    Changed message of api pricing scraper

--- api/pricing_scraper.py
+++ api/pricing_scraper.py
@@ -24,7 +24,7 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps({"status": "success", "message": "Benchmark completed successfully"}).encode())
+            self.wfile.write(json.dumps({"status": "success", "message": "Pricing scraper completed successfully"}).encode())
         except Exception as e:
             self.send_response(500)
             self.send_header('Content-type', 'application/json')
